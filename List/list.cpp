22834ae69c06bcedc3af51856d71dc3e35fedb4e
Юрій Краснюк <youra663@kpi.ua>
2022-03-24 22:22:19 +0200

    added declaration and description to methods of List-class

---
+++ List/list.cpp
@@ -0,0 +1,36 @@
+//list.cpp -- realize List's methods
+#include "list.h"
+#include <iostream>
+
+bool List::is_empty() const
+{
+	return count == 0;
+}
+
+bool List::is_full() const
+{
+	return count == MAX;
+}
+
+List::List()
+{
+	count = 0;
+}
+
+void List::add(const Item& element)
+{
+	list[count++] = element;
+}
+
+void List::show() const
+{
+	std::cout << "\tList:\n";
+	for (int i = 0; i < count; i++)
+		std::cout << "#" << i + 1 << ": " << list[i] << std::endl;
+}
+
+void List::visit(Item (*pf)(Item&))
+{
+	for (int i = 0; i < count; i++)
+		list[i] = pf(list[i]);
+}
---
+++ List/list.h
@@ -0,0 +1,24 @@
+//list.h -- declaration of List-class
+#ifndef LIST_H_
+#define LIST_H_
+//list will work with an "Items"
+typedef int Item;
+
+class List
+{
+private:
+	enum{MAX=10};				//limit of list
+	int count;			
+	Item list[MAX];
+public:
+	//create empty list
+	List();						
+	//~List();
+	bool is_empty() const;
+	bool is_full() const;
+	void add(const Item& element);
+	void show() const;
+	void visit(Item (*pf)(Item&));
+};
+
+#endif // !LIST_H_
